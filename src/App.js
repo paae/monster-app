968e8d0dc41d6209cd0691e2b72a8af7b3a68491
paae <67906440+paae@users.noreply.github.com>
2021-06-04 13:10:27 +0300

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import CardList from './component/card-list/card-list-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBox from './component/search-box/search-box';
+
+class App extends Component {
+ 
+
+
+  state = { 
+    monsters:[ ],
+    searchField:''
+  }
+
+ 
+ 
+//fetching data from api 
+//api request
+componentDidMount(){
+fetch("https://jsonplaceholder.typicode.com/users")
+
+.then(res =>res.json())
+.then (result=> this.setState({monsters: result}));
 }
 
+handleChange=(e)=>{
+  this.setState({searchField:e.target.value})
+}
+  
+  render() { 
+    const {searchField,monsters}=this.state
+    const filteredMonsters=monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
+    return (  
+    <div className="App">
+    <h1>monster Rolodex</h1>
+    <SearchBox placeholder="search monster" 
+    handleChange={this.handleChange}
+     />
+    
+    <CardList monsters={filteredMonsters}/>
+
+
+    </div>);
+  }
+}
+ 
 export default App;
---
+++ src/component/card-list/card-list-component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card-list.css'
+import Card from './../card/card';
+
+const CardList = ({monsters}) => {
+
+  return (
+
+    <div className="card-list ">
+    
+    {monsters.map((monster)=>(
+      <Card key={monster.id} monster={monster}/>
+    ))}</div>
+    );
+} 
+ 
+export default CardList;
